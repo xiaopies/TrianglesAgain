9ad217534303235befb4b6518dc6fe15d6ae94e9
xiaopies <xiaoshenma2016@gmail.com>
2020-10-30 03:19:15 -0400

    added test cases

--- Tester.java
+++ Tester.java
@@ -1,5 +1,6 @@
 public class Tester{
   public static void main(String[]args){
+
     Point a = new Point(2, 1);
     Point b = new Point(4, 8);
     Point c = new Point(8, 5);
@@ -12,11 +13,26 @@ public class Tester{
     Point j = new Point(3, 0);
     Point k = new Point(0, 3*Math.sqrt(3));
 
-   Triangle first = new Triangle(a, b, c);
-   Triangle second = new Triangle(d, e, f);
-   Triangle third = new Triangle(i, j, k);
-   System.out.println(third);
+    Triangle first = new Triangle(a, b, c);
+    Triangle second = new Triangle(d, e, f);
+    Triangle third = new Triangle(i, j, k);
+    System.out.println(third);
 
+    System.out.println("distance: " + a.distanceTo(b));
+    System.out.println("class: " + first.classify());
+    System.out.println("perimeter: " + first.getPerimeter());
+    System.out.println("area: " + first.getArea());
 
+    System.out.println("distance: " + d.distanceTo(e));
+    System.out.println("class: " + second.classify());
+    System.out.println("perimeter: " + second.getPerimeter());
+    System.out.println("area: " + second.getArea());
+
+    System.out.println("distance: " + g.distanceTo(h));
+    System.out.println("class: " + third.classify());
+    System.out.println("perimeter: " + third.getPerimeter());
+    System.out.println("area: " + third.getArea());
+
+  }
 
 }
